f46af15ba06aeb889c9eb8bb34eb540f480f2aa0
stevessr <89645372+stevessr@users.noreply.github.com>
2023-07-24 11:04:15 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'stevessr Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2141398-c25ff34af4bdd15b2e187e0f',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
